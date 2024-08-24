a2987d79a99918e0ff0d55e210a610e64f460783
Hyejin <hyejin.frontend@gmail.com>
2024-08-25 00:14:28 +0900

    design: GlobalStyle 및 theme 파일 추가 (#2)
    - GlobalStyle.tsx 파일에 전역 스타일 추가
    - theme.ts 파일에 전역 테마 추가
    - main.tsx 파일에 theme 및 GlobalStyle 적용
    - index.html 파일에 lang 속성 변경, title 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react';
 
+import { ThemeProvider } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import theme from '@/styles/\btheme';
+import GlobalStyle from '@/styles/GlobalStyle';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
