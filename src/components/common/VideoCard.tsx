f4f2eed7d3d94d251678cae0a974d39dc92b1ea9
miniseungmin <liontmdals@gmail.com>
2024-08-27 18:36:07 +0900

    feat: VideoCar 컴포넌트 (#28)

---
+++ src/components/common/VideoCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { css } from '@emotion/react';
+
+import Badge from '@/components/common/Badge';
+
+interface VideoCardProps {
+  onClick?: () => void;
+  thumbURL?: string;
+  time?: string;
+  width?: string;
+  height?: string;
+  type: 'main' | 'underbar';
+}
+
+const VideoCard: React.FC<VideoCardProps> = ({ onClick, thumbURL, time, type, width, height }) => {
+  const cardStyle = css`
+    margin: 1rem;
+    position: relative;
+    cursor: pointer;
+    overflow: hidden;
+    border-radius: 8px;
+    width: ${type === 'main' ? '145px' : width || 'auto'};
+    height: ${type === 'main' ? '85px' : height || 'auto'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.3);
+  `;
+
+  const imageStyle = css`
+    height: 100%;
+    width: auto;
+    object-fit: cover;
+  `;
+
+  return (
+    <div css={cardStyle} onClick={onClick}>
+      <img src={thumbURL} alt='Video thumbnail' css={imageStyle} />
+      {type === 'main' && time && <Badge position='corner'>{time}</Badge>}
+    </div>
+  );
+};
+
+export default VideoCard;
--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
-
 import ThumBox from '@/components/common/ThumBox';
-// import YouTubePlayerV3 from '@/components/YouTubePlayerV3';
-//import Profile from '@/components/profile/Profile';
-//import YouTubePlayerV3 from '@/components/YouTubePlayerV3';
+import VideoCard from '@/components/common/VideoCard';
+import Profile from '@/components/profile/Profile';
+import YouTubePlayerV3 from '@/components/YouTubePlayerV3';
 import ExampleTanStackQuery from '@/ExampleTanStackQuery';
 import useBearStore from '@/store/store';
 
@@ -27,19 +26,16 @@ const ExamplePage = () => {
         />
         <button onClick={() => setBear(value)}>상태 업데이트</button>
       </div>
-
       <div>
         <h1>텐스택쿼리 테스트</h1>
         <ExampleTanStackQuery />
       </div>
-
-      {/*
       <YouTubePlayerV3 videoId='WWHr-z6PZB0' />
       <Profile nickname='김승민32ㄴㅇㅎㅁㅇㄴㅎ' />
       <Profile
         nickname='mini'
         profileImageSrc='https://img.freepik.com/free-vector/young-man-with-blue-hair_24877-82124.jpg?t=st=1724720053~exp=1724723653~hmac=2deb5619e93e7a773e2d7f182144cc8c65fa620d252c35388c2f3ec5adac104e&w=1480'
-*/}
+      />
       <ThumBox
         type='main1'
         thumURL='https://goodsisgood.com/wp-content/uploads/2024/02/mindaday1.jpg'
@@ -67,6 +63,17 @@ const ExamplePage = () => {
         uploader='김아무개'
         update='2일 전에 업데이드 됨'
         listnum='3'
+      />
+      <VideoCard
+        type='main'
+        thumbURL='https://goodsisgood.com/wp-content/uploads/2024/02/mindaday1.jpg'
+        time='3:45'
+      />
+      <VideoCard
+        type='underbar'
+        width='100px'
+        height='50px'
+        thumbURL='https://goodsisgood.com/wp-content/uploads/2024/02/mindaday1.jpg'
       />
     </div>
   );
