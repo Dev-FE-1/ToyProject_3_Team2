59a1f7285262ab620b9ab28a25ab4ec04f18ce88
Hyejin <hyejin.frontend@gmail.com>
2024-08-30 10:08:09 +0900

    feat: 마이페이지 Playlists 컴포넌트 구현 (#72)
    - 플레이리스트 총 갯수 보여주기
    - 처음 화면 렌더링 되면 내가 가진 모든 플레이리스트 그리드 형식으로 보여주기
    - 플립카드 클릭하면 뒷면으로 뒤집어지고, 뒷면의 자세히 보기 버튼 누르면 해당 플레이리스트 메인화면으로 이동
    - 플립카드 클릭 후, 다른 플립카드 클릭하면 -> 이전에 뒤집어진 플립카드는 원상복귀
    - 토글 활성 화 시, 공개상태의 플레이리스트만 필터링됨
    - 만약 토글 활성화 전에 한개이상 플립카드가 뒷면상태였다면, 토글활성화 동시에 플립카드 모두 앞면으로 보여지고, 공개 플리만 필터링
    - 플리는 모두 n열 3행으로 보여지고 플립카드의 부모 컴포넌트의 가로사이즈를 받아서 꽉 채워짐

---
+++ src/types/playlist.d.ts
@@ -0,0 +1,21 @@
+export interface Playlist {
+  playlistId: string;
+  userId: string;
+  title: string;
+  description: string;
+  category: string;
+  createdAt: string;
+  updatedAt: string;
+  likeCount: number;
+  forkCount: number;
+  commentCount: number;
+  videoCount: number;
+  thumbnailUrl: string;
+  isPublic: boolean;
+  videos: Array<{
+    videoId: string;
+    title: string;
+    thumbnailUrl: string;
+    duration: number;
+  }>;
+}
