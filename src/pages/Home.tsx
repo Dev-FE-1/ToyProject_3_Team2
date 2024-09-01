74cf1e268375b2627d82f687d72a33228a85db80
hyojeong <okm0227@naver.com>
2024-09-01 20:37:26 +0900

    feat: 홈 퍼블리싱 타이틀 추가
    홈 퍼블리싱 작업중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,8 @@
+import { css } from '@emotion/react';
+import { RiArrowRightSLine } from 'react-icons/ri';
+
 import SelectBox from '@/components/common/SelectBox';
+import theme from '@/styles/theme';
 
 // SelectBox에 들어갈 내용
 const items = [
@@ -14,12 +18,35 @@ const items2 = [
 
 const Home = () => (
   <div>
-    <h1>Logo</h1>
-    <p>Home</p>
+    <img src='/logo.svg' alt='Logo' css={logoStyle} />
+    <div css={titleStyle}>
+      <p>내 관심사와 비슷한 플레이리스트</p>
+      <RiArrowRightSLine />
+    </div>
+
     <p>selectbox 테스트</p>
     <SelectBox items={items} />
     <SelectBox items={items2} />
   </div>
 );
+
+const logoStyle = css`
+  width: 120px;
+  margin: 2rem 0 1rem 1rem;
+`;
+
+const titleStyle = css`
+  display: flex;
+  align-items: center;
+  font-size: ${theme.fontSizes.normal};
+  font-weight: 700;
+  gap: 10px;
+
+  svg {
+    width: 20px;
+    height: 20px;
+    color: ${theme.colors.disabled};
+  }
+`;
 
 export default Home;
