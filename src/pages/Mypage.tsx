f4b45fd890febd07b77b93279da34097d8a794a6
Hyejin <hyejin.frontend@gmail.com>
2024-09-01 21:09:34 +0900

    feat: 새 재생목록 만들기 페이지 추가 후 Mypage 페이지에 연결 및 라우터 설정 (#85)
    - 새로운 기능인 '새 재생목록 만들기 페이지'를 추가
    - Mypage 페이지에 '새 재생목록 만들기 페이지'로 이동할 수 있는 버튼 추가
    - 라우터 설정을 통해 '새 재생목록 만들기 페이지'로 이동할 수 있도록 함

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
 
 import { css } from '@emotion/react';
+import { RiAddLargeLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
 
 import { getAllPlaylists } from '@/api/playlist';
+import IconButton from '@/components/common/buttons/IconButton';
+import Toast from '@/components/common/Toast';
 import MyPlaylists from '@/components/mypage/MyPlaylists';
 import MyProfile from '@/components/mypage/MyProfile';
-import { Playlist } from '@/types/playlist';
+import { PATH } from '@/constants/path';
+import { PlaylistModel } from '@/types/playlist';
 
 const MyPage = () => {
-  const [playlists, setPlaylists] = useState<Playlist[]>([]);
+  const navigate = useNavigate();
+  const [playlists, setPlaylists] = useState<PlaylistModel[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
   useEffect(() => {
@@ -25,16 +31,38 @@ const MyPage = () => {
     };
     fetchPlaylists();
   }, []);
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  const handleAddPlaylist = () => {
+    navigate(`${PATH.MYPAGE}/${PATH.MYPAGE_ADD_PLAYLIST}`);
+  };
+
   return (
-    <div css={containerStyle}>
-      <MyProfile />
-      <MyPlaylists playlists={playlists} />
-    </div>
+    <>
+      <div css={containerStyle}>
+        <MyProfile />
+        <MyPlaylists playlists={playlists} />
+        <IconButton
+          Icon={RiAddLargeLine}
+          customStyle={floatAddButtonStyle}
+          onClick={handleAddPlaylist}
+        />
+      </div>
+      <Toast />
+    </>
   );
 };
 const containerStyle = css`
+  position: relative;
+  max-width: 498px;
   padding-bottom: 80px;
+`;
+const floatAddButtonStyle = css`
+  position: fixed;
+  right: 1rem;
+  bottom: 96px;
+  z-index: 100;
+  transition: all 0.3s ease;
+  &:hover {
+    transform: translateY(-2px);
+  }
 `;
 export default MyPage;
