8c3b066a6e4847295d217fd93bdd470b95c2459d
LeeYunHwan <cyzhqly@gmail.com>
2024-09-02 09:50:33 +0900

    chore: 구독하기 토글 전역상태로 관리하도록 변경 (#58)
    구독하기 토글 상태관리 useToggleStore로 변경

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react';
-
 import { css } from '@emotion/react';
 
 import Toast from '@/components/common/Toast';
 import PlaylistBox from '@/components/playlist/PlaylistBox';
 import useToastStore from '@/store/useToastStore';
+import useToggleStore from '@/store/useToggleStore';
 import theme from '@/styles/theme';
 
 const Subscriptions = () => {
-  const [isSubscribed, setIsSubscribed] = useState(false);
+  const { isToggled, toggle } = useToggleStore();
   const showToast = useToastStore((state) => state.showToast);
 
   const handleSubBtnClick = () => {
-    setIsSubscribed(!isSubscribed);
-    isSubscribed
+    toggle();
+    isToggled
       ? showToast('구독 목록에서 해제되었습니다.')
       : showToast('구독 목록에 추가되었습니다.');
   };
@@ -24,21 +23,21 @@ const Subscriptions = () => {
         <p>내가 구독중인 플레이리스트</p>
       </header>
       <PlaylistBox
-        isSubscribed={isSubscribed}
+        isSubscribed={isToggled}
         onClick={handleSubBtnClick}
         nickname='고먐미'
         imageUrl='https://goodsisgood.com/wp-content/uploads/2024/02/mindaday1.jpg'
         playlistTitle='개쩌는 플레이리스트 볼사람 여기여기 모여라'
       />
       <PlaylistBox
-        isSubscribed={isSubscribed}
+        isSubscribed={isToggled}
         onClick={handleSubBtnClick}
         nickname='고먐미'
         imageUrl='https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA0MTBfMzMg%2FMDAxNzEyNzA2NTYzMzQ2.LJjblzvIEqxPO_5qBiB4Sk4RtCMhhySYiPgsrtUrp24g.5LiOVYy3D4ZuKA9NEWFPHBvpDv-i-gai52dRszy9DhMg.JPEG%2F1.jpg&type=sc960_832'
         playlistTitle='국뽕 치사량 최대치로 올라오는 플리 모음 1탄'
       />
       <PlaylistBox
-        isSubscribed={isSubscribed}
+        isSubscribed={isToggled}
         onClick={handleSubBtnClick}
         nickname='고먐미'
         imageUrl='https://static.displate.com/857x1200/displate/2023-07-04/8bdb31c1949b22406cb2a9c257dae6f4_45fcb4a5a3d57eab739d4a610fb77ab2.jpg'
