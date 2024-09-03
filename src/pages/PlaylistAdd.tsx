7fd5ddfd9784a3db7cf9971f81b5d512289cb254
Hyejin <hyejin.frontend@gmail.com>
2024-09-04 08:03:04 +0900

    fix: 새 재생목록 생성 시, 로그인 한 user의 userId와 userName 추가하여 저장

--- src/pages/PlaylistAdd.tsx
+++ src/pages/PlaylistAdd.tsx
@@ -1,22 +1,49 @@
 import { useNavigate } from 'react-router-dom';
 
 import { addPlaylist } from '@/api/endpoints/playlist';
+import { getUserData } from '@/api/endpoints/user';
 import Toast from '@/components/common/Toast';
 import PlaylistForm from '@/components/playlistForm/PlaylistForm';
 import { PATH } from '@/constants/path';
 import Header from '@/layouts/layout/Header';
 import { useToastStore } from '@/store/useToastStore';
-import { PlaylistFormDataModel } from '@/types/playlist';
+import { PlaylistFormDataModel, PlaylistModel } from '@/types/playlist';
 
 const PlaylistAdd = () => {
   const navigate = useNavigate();
   const showToast = useToastStore((state) => state.showToast);
 
-  const handleSubmit = (data: PlaylistFormDataModel) => {
-    console.log(data);
+  const handleSubmit = async (formData: PlaylistFormDataModel) => {
+    console.log(formData);
     try {
+      // 세션 스토리지에서 userSession 문자열을 가져와서 파싱
+      const userSessionStr = sessionStorage.getItem('userSession');
+      if (!userSessionStr) {
+        throw new Error('세션에 유저 ID를 찾을 수 없습니다.');
+      }
+      const userSession = JSON.parse(userSessionStr);
+      const userId = userSession.uid;
+
+      if (!userId) {
+        throw new Error('유효한 사용자 ID가 없습니다.');
+      }
+
+      // 사용자 데이터 가져오기
+      const user = await getUserData(userId);
+      if (!user) {
+        throw new Error('사용자 데이터를 가져오는 데 실패했습니다.');
+      }
+
+      // 플레이리스트 데이터에 사용자 정보를 추가
+      const playlistData: PlaylistModel = {
+        ...formData,
+        userId: user.userId,
+        userName: user.userName,
+        playlistId: '', // 플레이리스트 ID는 추가 시 자동 생성
+      };
+
       // 플레이리스트 추가 API 호출
-      const playlistId = addPlaylist(data);
+      const playlistId = addPlaylist(playlistData);
       console.log('추가된 플레이리스트 ID:', playlistId);
       // 성공 시 마이페이지로 이동
       showToast('플레이리스트가 추가되었습니다.');
