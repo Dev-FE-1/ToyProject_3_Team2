b1e7ae469a4c7b9829dd6aa764dba870754b9d93
Hyejin <hyejin.frontend@gmail.com>
2024-09-04 06:36:17 +0900

    feat: 플레이리스트 메인 페이지 구현
    - 전달받은 playlistId를 이용해 플레이리스트 정보와 유저정보를 가져오는 API 호출
    - 플레이리스트 정보와 유저정보를 화면에 렌더링
    - 생성한 플레이리스트 안에 영상이 없을 시 보여질 화면 구현

--- src/pages/PlayList.tsx
+++ src/pages/PlayList.tsx
@@ -1,69 +1,102 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { css } from '@emotion/react';
-import { GoStar, GoStarFill, GoKebabHorizontal } from 'react-icons/go';
-import { RiPlayLargeFill } from 'react-icons/ri';
+import { GoKebabHorizontal, GoStar, GoStarFill } from 'react-icons/go';
+import { RiPlayLargeFill, RiPencilFill, RiPencilLine } from 'react-icons/ri';
+import { useParams } from 'react-router-dom';
 
+import { getPlaylistWithUser } from '@/api/endpoints/playlist';
+import defaultProfileImage from '@/assets/images/default-avatar-man.svg';
 import Button from '@/components/common/buttons/Button';
 import IconButton from '@/components/common/buttons/IconButton';
+import Spinner from '@/components/common/Spinner';
 import Toast from '@/components/common/Toast';
 import ThumBoxDetail from '@/components/playlistdetail/thumBoxDetail';
 import VideoBoxDetail from '@/components/playlistdetail/vedieoBoxDetail';
 import Header from '@/layouts/layout/Header';
 import { useToastStore } from '@/store/useToastStore';
+import { useToggleStore } from '@/store/useToggleStore';
 import theme from '@/styles/theme';
 import { PlaylistModel } from '@/types/playlist';
+import { UserModel } from '@/types/user';
 
-const fetchMyPlaylists = async (): Promise<PlaylistModel[]> => {
-  const response = await fetch('/src/mock/playlists.json');
-  return await response.json();
-};
-
-const PlayListPage = () => {
-  const [isStarFilled, setIsStarFilled] = useState(false);
+const PlaylistPage: React.FC = () => {
+  const { playlistId } = useParams<{ playlistId: string }>(); // URL 파라미터에서 playlistId 추출
   const [playlist, setPlaylist] = useState<PlaylistModel | null>(null);
+  const [user, setUser] = useState<UserModel | null>(null);
+  const isToggled = useToggleStore((state) => state.isToggled);
+  const toggle = useToggleStore((state) => state.toggle);
   const showToast = useToastStore((state) => state.showToast);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const loadPlaylist = async () => {
-      try {
-        const playlists = await fetchMyPlaylists();
-        if (playlists.length > 0) {
-          setPlaylist(playlists[2]);
-        }
-      } catch (error) {
-        console.error('Failed to load playlist:', error);
+    async function fetchPlaylistWithUser() {
+      if (!playlistId) {
+        setError('Playlist ID is missing');
+        setIsLoading(false);
+        return;
       }
-    };
 
-    loadPlaylist();
-  }, []);
+      try {
+        const result = await getPlaylistWithUser(playlistId);
+
+        if (result) {
+          setPlaylist(result.playlist);
+          setUser(result.user);
+        } else {
+          setError('Playlist not found');
+        }
+      } catch (err) {
+        console.error('Error fetching playlist:', err); // 로그 추가
+        setError('Failed to fetch playlist');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchPlaylistWithUser();
+  }, [playlistId]);
 
   const handleIconButtonClick = () => {
-    setIsStarFilled(!isStarFilled);
-    if (!isStarFilled) {
-      showToast('내 재생목록에 저장되었습니다.');
-    }
+    showToast('내 재생목록에 저장되었습니다.');
+    toggle();
   };
 
-  if (!playlist) {
-    return <div>Loading...</div>;
+  if (isLoading) {
+    return (
+      <div css={spinnerContainerStyle}>
+        <Spinner />
+      </div>
+    );
+  }
+  if (!playlist || !user) {
+    return (
+      <div>
+        <Header customStyle={kebabStyle} />
+        <div css={nullContentStyle}>
+          <div>앗! 아직 영상이 없어요</div>
+          <div>영상을 추가하여</div>
+          <div>나만의 플리를 만들어보세요</div>
+        </div>
+      </div>
+    );
+  }
+  if (error) {
+    return <div>{error}</div>;
   }
 
   return (
     <div css={containerStyle}>
-      <Header
-        Icon={GoKebabHorizontal}
-        onBack={() => {
-          console.log('사용자정의 뒤로가기 동작 추가');
-        }}
-        customStyle={kebabStyle}
-      />
-      <ThumBoxDetail
-        playlist={playlist}
-        profileURL='https://img.freepik.com/free-vector/young-man-with-blue-hair_24877-82124.jpg?t=st=1724720053~exp=1724723653~hmac=2deb5619e93e7a773e2d7f182144cc8c65fa620d252c35388c2f3ec5adac104e&w=1480'
-        onClickProfile={() => console.log('프로필 클릭')}
-      />
+      <Header Icon={GoKebabHorizontal} customStyle={kebabStyle} />
+      {playlist && (
+        <ThumBoxDetail
+          playlist={playlist}
+          user={user}
+          profileURL={user.profileImg || defaultProfileImage}
+          onClickProfile={() => console.log('프로필 클릭')}
+        />
+      )}
       <div css={buttonBoxStyle}>
         <Button
           styleType='secondary'
@@ -73,15 +106,21 @@ const PlayListPage = () => {
           <RiPlayLargeFill css={iconStyle} />
           Play all
         </Button>
-        <IconButton Icon={isStarFilled ? GoStarFill : GoStar} onClick={handleIconButtonClick} />
-        {/* <IconButton Icon={RiPencilLine} onClick={() => console.log('편집모달')} /> */}
+        {playlist.userId === user.userId ? (
+          <IconButton
+            Icon={isToggled ? RiPencilFill : RiPencilLine}
+            onClick={handleIconButtonClick}
+          />
+        ) : (
+          <IconButton Icon={isToggled ? GoStarFill : GoStar} onClick={handleIconButtonClick} />
+        )}
       </div>
       {playlist.videos.map((video) => (
         <VideoBoxDetail
           key={video.videoId}
           video={video}
-          type='host' // 아이디 비교해서 값이 참이면 host 다르면 visitor
-          channelName={playlist.userId}
+          type={playlist.userId === user.userId ? 'host' : 'visitor'} // 아이디 비교해서 값이 참이면 host 다르면 visitor
+          channelName={playlist.userName}
           uploadDate={new Date(playlist.createdAt).toLocaleDateString()}
           onClick={() => console.log(`비디오 클릭됨: ${video.videoId}`)}
           onClickKebob={(e) => console.log('kebab 아이콘 클릭', video.videoId)}
@@ -91,11 +130,12 @@ const PlayListPage = () => {
     </div>
   );
 };
-
 const containerStyle = css`
-  padding-bottom: 80px;
+  padding-bottom: 90px;
 `;
-
+const kebabStyle = css`
+  transform: rotate(90deg);
+`;
 const buttonBoxStyle = css`
   margin: 1rem;
   display: flex;
@@ -114,7 +154,22 @@ const iconStyle = css`
   margin-right: 6px;
 `;
 
-const kebabStyle = css`
-  transform: rotate(90deg);
+const spinnerContainerStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
-export default PlayListPage;
+const nullContentStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-top: 50%;
+  height: 100vh;
+  div {
+    text-align: center;
+    margin: 1rem;
+  }
+`;
+
+export default PlaylistPage;
