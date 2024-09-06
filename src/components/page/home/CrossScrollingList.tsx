5f766fe57fdf5d3ae3b7be451df67d8ac0b14962
hyojeong <okm0227@naver.com>
2024-09-07 00:30:41 +0900

    fix: 미사용한 코드 제거 (#163)

--- src/components/page/home/CrossScrollingList.tsx
+++ src/components/page/home/CrossScrollingList.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import { css } from '@emotion/react';
 import { RiAddLargeLine, RiArrowRightSLine } from 'react-icons/ri';
