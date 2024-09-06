bb11ddd59d9a79bf8b719e953ca8d225948d5ff9
hyojeong <okm0227@naver.com>
2024-09-07 01:10:17 +0900

    feat: 홈에서 최신 플리 무한스크롤 5개씩 데이터 불러오기
    홈에 있는 최신업데이트 플리에서 무한스크롤로 5개씩 데이터를 받아옵니다

--- src/components/page/home/RecentUpdateList.tsx
+++ src/components/page/home/RecentUpdateList.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+
 import { css } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 
 import ThumbNailBox from '@/components/common/ThumbNailBox';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 import theme from '@/styles/theme';
 import { PlaylistModel } from '@/types/playlist';
 import { formatTimeWithUpdated } from '@/utils/formatDate';
@@ -13,11 +16,23 @@ interface RecentUpdateListProps {
 
 const RecentUpdateList: React.FC<RecentUpdateListProps> = ({ title, playlists }) => {
   const navigate = useNavigate();
+  const [visiblePlaylists, setVisiblePlaylists] = useState<PlaylistModel[]>(playlists.slice(0, 10)); // 처음 10개의 항목만 표시
+  const [page, setPage] = useState(1);
+
+  const loadMoreItems = () => {
+    console.log('Load more items');
+    const nextPage = page + 1;
+    const newVisiblePlaylists = playlists.slice(0, nextPage * 5); // 다음 10개의 항목을 가져옴
+    setVisiblePlaylists(newVisiblePlaylists);
+    setPage(nextPage);
+  };
+
+  const targetRef = useInfiniteScroll(loadMoreItems);
 
   return (
     <div>
       <h2 css={titleStyle}>{title}</h2>
-      {playlists.map((playlist) => (
+      {visiblePlaylists.map((playlist) => (
         <div key={playlist.playlistId} onClick={() => navigate(`playlist/${playlist.playlistId}`)}>
           <ThumbNailBox
             type='details'
@@ -32,6 +47,9 @@ const RecentUpdateList: React.FC<RecentUpdateListProps> = ({ title, playlists })
           />
         </div>
       ))}
+
+      {/* 무한 스크롤 트리거 요소 */}
+      <div ref={targetRef} style={{ height: '20px', backgroundColor: 'transparent' }} />
     </div>
   );
 };
